c032cc1977ee5f595d01870cd230c2a3e8ee7d23
clb <clb@10.39.192.115>
2017-12-28 18:10:09 +0800

    dev

--- src/main/java/com/eureka/server/EurekaServer.java
+++ src/main/java/com/eureka/server/EurekaServer.java
@@ -11,10 +11,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServer 
-{
-    public static void main( String[] args )
-    {
-    	 SpringApplication.run(EurekaServer.class, args);
-    }
+public class EurekaServer {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer.class, args);
+	}
 }
